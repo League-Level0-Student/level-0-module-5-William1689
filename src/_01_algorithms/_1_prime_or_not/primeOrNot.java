dc3b62fd30acad3075a63f9eab4bfec9cd1bee17
league <richardson.liou@gmail.com>
2023-02-20 17:04:20 -0800

    I finished mod 4

---
+++ src/_01_algorithms/_1_prime_or_not/primeOrNot.java
@@ -0,0 +1,30 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class primeOrNot {
+	public static void main(String[] args) {
+int divisor = 2;
+String primeString = JOptionPane.showInputDialog("Give me a number");
+int primeNum = Integer.parseInt(primeString);
+while(divisor>17) {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
