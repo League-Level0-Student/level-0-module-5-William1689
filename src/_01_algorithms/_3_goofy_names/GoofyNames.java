1be21caf404006b1ecf8792159b7376badcb5124
league <ameeralih786@gmail.com>
2023-03-06 16:58:30 -0800

    I DID MOD 5

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -8,13 +8,13 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		//String goofyName = "";
+		String Name = "";
 
 		// 1. Ask the user to enter their name
 		
-		String gOoFyNaMe = JOptionPane.showInputDialog("Enter your name");
+		String goofyname = JOptionPane.showInputDialog("Enter your name");
 		int goofy = 1;
-		int lenght = gOoFyNaMe.length();
+		int lenght = goofyname.length();
 		int times= 0;
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
@@ -24,13 +24,18 @@ public class GoofyNames {
 
 				// 4. Create a char variable to store the next character of the name
 				//    use .charAt()
+		
 		while(times<lenght) {
-			char name = gOoFyNaMe.toUpperCase().charAt(goofy);
-			goofy+=2;
-			times+=0;
-		}
-			String Str = ("") ;
-		// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
+			if(times % 2 == 0) {
+				char c = goofyname.charAt(times);
+				Name = Name+Character.toUpperCase(c);
+				}
+			else {
+				char c = goofyname.charAt(times);
+				Name = Name+Character.toLowerCase(c);
+			}
+			times+=1;
+			// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
 			
 			
 				// 6. Even characters should be made uppercase and odd characters made lowercase
@@ -38,8 +43,8 @@ public class GoofyNames {
 	
 			
 				// 7. ADD the char to the end of the goofyName String
-
-		
+		}
+			JOptionPane.showMessageDialog(null, Name);
 		// 8. Use pop-up to show user their Goofy name
 
 	}
