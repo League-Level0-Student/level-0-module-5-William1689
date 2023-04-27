5aa08eaac25b64e720184a15bc039713e62030f1
league <ameeralih786@gmail.com>
2023-04-27 16:59:05 -0700

    I did a worksheet

---
+++ src/_04_practice/_1_skill_practice/Skill.java
@@ -0,0 +1,90 @@
+package _04_practice._1_skill_practice;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Skill {
+	public static void main(String[] args) {
+//   Skill 1{
+//String dimes = JOptionPane.showInputDialog("How many dimes do you have");
+//int cents = Integer.parseInt(dimes);
+//JOptionPane.showMessageDialog(null, cents*10+ " cents"); 	
+	
+//String height = JOptionPane.showInputDialog("How tall are you (inches)");
+//int tall = Integer.parseInt(height);
+//if(tall<36) {
+//	JOptionPane.showMessageDialog(null, "Eat your wheaties");
+//} }
+		
+//Skill 2{
+//for(int i = 1; i<31;i+=3) {
+//System.out.println(i);
+//} }
+	
+//Skill 3{
+//int number = 0;
+//Random ran = new Random();
+//number = ran.nextInt(10);
+//JOptionPane.showMessageDialog(null, number);
+		
+//String num = JOptionPane.showInputDialog("Give me a number");
+//String num1 = JOptionPane.showInputDialog("Give me another number");
+//int i = Integer.parseInt(num);
+//int n = Integer.parseInt(num1);
+//int x = i-n;
+//JOptionPane.showMessageDialog(null, "The difference between these two numbers is "+x);}
+		
+//Skill 4{
+
+//String city = JOptionPane.showInputDialog("Which city do you live in?");
+//if(city.equalsIgnoreCase("San Diego")){
+	//JOptionPane.showMessageDialog(null, "You live in the worlds finest city");
+//}
+//else {
+//JOptionPane.showMessageDialog(null, "Move to San Diego");
+//}	
+
+String cars = JOptionPane.showInputDialog("How many cars do you have?");
+int car = Integer.parseInt(cars);
+if(car==0) {
+	JOptionPane.showMessageDialog(null, "You probaly use public transportation");
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
